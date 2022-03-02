ddafce2432cc36b15b32db2564c17679c698a447
Pavel Isakov <isapawel@gmail.com>
2022-03-02 23:10:13 +0700

    Initial Commit

---
+++ corDatProjct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  corDatProjct
+//
+//  Created by Pavel Isakov on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
